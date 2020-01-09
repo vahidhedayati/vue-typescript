9bf41e49565edc78f58c3e2b892f548ab765fbd6
Vahid Hedayati <badvad@gmail.com>
2020-01-09 10:15:32 +0000

    working basics

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-   <button onclick="incrementCounter">Increment</button>{{counter}}
+   <button @click="incrementCounter()">Increment</button>{{counter}}
 
    {{fullName}}
   </div>
@@ -13,11 +13,15 @@ import { Component, Prop, Vue } from 'vue-property-decorator';
 @Component
 export default class HelloWorld extends Vue {
   @Prop() private msg!: string;
-  @Prop() private counter: number=1;
-  @Prop() private firstName: String;
-  @Prop() private lastName: String;
+  //@Prop() private counter: number=1;
+  counter=1;
+  firstName='';
+  lastName='';
+  //@Prop() private firstName!: String;
+  //@Prop() private lastName!: String;
 
   incrementCounter() {
+    console.log('incrementing')
     this.counter++
   }
 
